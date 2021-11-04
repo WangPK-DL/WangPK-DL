5c80cb684527e878ceccd7d4cb5515ad26a9bc18
pkcile <wangpengkun2012@hotmail.com>
2021-11-04 15:12:36 +0800

    第四次服务

---
+++ static/js/chunk-2d0ab43a.6ccf00a0.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ab43a"],{1511:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"user",staticStyle:{width:"100%",height:"100%"}},[n("router-view")],1)},a=[],i={},u=i,c=n("2877"),l=Object(c["a"])(u,s,a,!1,null,null,null);e["default"]=l.exports}}]);
